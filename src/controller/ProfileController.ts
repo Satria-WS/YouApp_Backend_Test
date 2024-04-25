628c0dcf36fb2ea8aa7d44ec7a631deda65abd8e
Satria-WS <extencezone02@gmail.com>
2024-04-26 00:29:23 +0700

    update crud controller

--- src/controller/ProfileController.ts
+++ src/controller/ProfileController.ts
@@ -1,8 +1,36 @@
+import { ProfileSchema } from '../Models/profile';
 import express from 'express';
 
 export const getAllProfiles = async (req: express.Request, res: express.Response) => {
   try {
-    return res.status(200).send("hello world")
+    const profile = await ProfileSchema.find();
+    // return res.status(200).send("hello world")
+    return res.status(200).json({
+      data: profile,
+      message:"get Profile succesfull"
+    })
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export const createProfile = async (req: express.Request, res: express.Response) => {
+  try {
+    const { displayName, gender, birthday, horoscope, zodiac, height, weight } = req.body;
+    if (!displayName || !gender || !birthday || !horoscope || !zodiac || !height || !weight) {
+      return res.sendStatus(400);
+    }
+
+    if (typeof displayName !== 'string' || typeof gender !== 'string' || typeof birthday !== 'string' || typeof horoscope !== 'string' || typeof zodiac !== 'string' || typeof height !== 'number' || typeof weight !== 'number') {
+      return res.status(400).json({ error: "Invalid data format" });
+    }
+    
+    const createProfile = await ProfileSchema.create(req.body);
+    return res.status(200).json({
+      data: createProfile,
+      message:"create profile sucessfull"
+    })
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
--- src/router/profile.ts
+++ src/router/profile.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { getAllProfiles } from "../controller/ProfileController";
+import { createProfile, getAllProfiles } from "../controller/ProfileController";
 import { isAuthenticated } from "../middlewares/index";
-
 
 export default (router: express.Router) => {
   router.get("/profiles", isAuthenticated, getAllProfiles);
+  router.post("/profiles", isAuthenticated, createProfile);
 };
