f49321cc83da848b5de47452a92300572798826c
Satria-WS <extencezone02@gmail.com>
2024-04-26 09:45:39 +0700

    fixing date request for profile

--- src/Models/profile.ts
+++ src/Models/profile.ts
@@ -1,4 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
+
+
 
 const profileModel = new mongoose.Schema({
   displayName: {
@@ -31,7 +33,53 @@ const profileModel = new mongoose.Schema({
     required: true
   }
 })
+
+// profileModel.pre('save', function (next) {
+//   const birthday = this.birthday;
+//   const month = birthday.getMonth() + 1; // getMonth() returns 0-based index
+//   const day = birthday.getDate();
+
+//     // Calculate horoscope
+//     const horoscopes = [
+//       "Capricornus (Goat)",
+//       "Aquarius (Water Bearer)",
+//       "Pisces (Fish)",
+//       "Aries (Ram)",
+//       "Taurus (Bull)",
+//       "Gemini (Twins)",
+//       "Cancer (Crab)",
+//       "Leo (Lion)",
+//       "Virgo (Virgin)",
+//       "Libra (Balance)",
+//       "Scorpius (Scorpion)",
+//       "Sagittarius (Archer)"
+//     ];
+//     const horoscopeIndex = (month + (day > 20 ? 1 : 0) - 1) % 12;
+//     this.horoscope = horoscopes[horoscopeIndex];
+  
+//     // Calculate zodiac
+//     const zodiacSigns = [
+//       "Capricornus",
+//       "Aquarius",
+//       "Pisces",
+//       "Aries",
+//       "Taurus",
+//       "Gemini",
+//       "Cancer",
+//       "Leo",
+//       "Virgo",
+//       "Libra",
+//       "Scorpio",
+//       "Sagittarius"
+//     ];
+//     const zodiacIndex = (month + (day > 20 ? 1 : 0) - 1) % 12;
+//     this.zodiac = zodiacSigns[zodiacIndex];
+  
+//     next();
+// })
 export const ProfileSchema = mongoose.model("Profile", profileModel);
+
+
 
 // const doc = new ProfileSchema({
 //   displayName: "John Doe",
